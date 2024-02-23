09c0d68377e27015e0f406dedfb14756a9c70b42
Gabriel H. Kierkegaard <gabkierk@proton.me>
2024-02-23 18:14:01 +0100

    Created main.cpp

---
+++ Part 1 - Up and Running/Sandbox/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <iostream>
+#include "step.h"
+using namespace std;
+
+int main()
+{
+  int the_answer = 42;
+  int lucky_number = the_answer / 6;
+  cout << "The lucky number is " << lucky_number << endl;
+
+  int x = lucky_number;
+
+  if (x > 0)
+  {
+    cout << "Positive." << endl;
+  }
+  else if (x < 0)
+  {
+    cout << "Negative." << endl;
+  }
+  else
+  {
+    cout << "Zero." << endl;
+  }
+
+  int result = step::step_function(lucky_number);
+  cout << "The result is " << result << endl;
+
+  return 0;
+}
